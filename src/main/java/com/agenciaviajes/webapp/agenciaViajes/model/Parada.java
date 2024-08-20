bc0ba39d685819ea344c01d3ff11f7d5141b8d85
aarbizu-2020398 <aarbizu-2020398@kinal.edu.gt>
2024-08-20 07:33:43 -0600

    Correccion Comentario para el merge

--- src/main/java/com/agenciaviajes/webapp/agenciaViajes/model/Parada.java
+++ src/main/java/com/agenciaviajes/webapp/agenciaViajes/model/Parada.java
@@ -1,14 +1,14 @@
 package com.agenciaviajes.webapp.agenciaViajes.model;
-
+ 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-
+ 
 @Entity
 @Data
 @Table (name="Paradas")
-
+ 
 public class Parada {
     @Id
     private Long id;
@@ -16,5 +16,5 @@ public class Parada {
     private String ubicacion;
     private String tiempo;
     private String tipoParada;
-
+ 
 }
