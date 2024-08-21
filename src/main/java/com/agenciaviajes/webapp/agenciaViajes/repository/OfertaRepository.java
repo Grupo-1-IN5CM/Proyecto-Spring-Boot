c9f7a01787ff72e06da00858a640700cd465d93d
GioCG <gcarrera-2020313@kinal.edu.gt>
2024-08-21 00:43:09 -0600

    Comentario personal

--- src/main/java/com/agenciaviajes/webapp/agenciaViajes/repository/OfertaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agenciaviajes.webapp.agenciaViajes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.agenciaviajes.webapp.agenciaViajes.model.Oferta;
-
-public interface OfertaRepository extends JpaRepository<Oferta,Long>{
-
-}
